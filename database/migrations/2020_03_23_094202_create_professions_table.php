e36220552393c2e026bb8a8dd6db2fcdd52dc4a4
jaimejunquera <junque96@gmail.com>
2020-03-19 14:05:02 +0100

    Migrations DB

---
+++ database/migrations/2020_03_23_094202_create_professions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfessionsTable extends Migration
+{
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professions', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title',100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professions');
+    }
+}
