fa076c48624ab059e80db8eaaa0881117b9b424c
jaimejunquera <junque96@gmail.com>
2020-03-19 14:05:02 +0100

    Importing my project to Git, without saving history.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
 });
+
+Route::get('/usuarios', 'UserController@index');
+
+Route::get('/usuarios/{id}', 'UserController@show')
+    ->where('id', '[0-9]+');
+
+Route::get('/usuarios/nuevo', 'UserController@create');
+
+Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
